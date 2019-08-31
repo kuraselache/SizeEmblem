e868146b082a6a6a20c8467f5e3864e3450f63e4
kuraselache <54523530+kuraselache@users.noreply.github.com>
2019-08-31 00:40:48 -0600

    Cleanup & MapPoint
    * Removed from Grid prefab that wasn't being used, need to keep things from getting too messy
    
    * Made a MapPoint object with the goal of being able to represent an object on the map of varying dimensions and testing collisions/intersections with other map points

---
+++ Size Emblem Project/Assets/Scripts/Containers/MapPoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeEmblem.Scripts.Containers
+{
+    public struct MapPoint
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+
+
+        public MapPoint(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+
+        public bool Intersect(MapPoint other)
+        {
+            if(Width == 1 && other.Width == 1 && Height == 1 && other.Height == 1)
+
+            if ((X + Width < other.X || other.X + other.Width < X) && (Y + Height < other.Y || other.Y + other.Height < Y)) return false;
+            return true;
+        }
+    }
+}
